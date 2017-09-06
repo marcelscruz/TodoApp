4db517d716e7f0201b563ab30cc6056fa93afdc7
Marcel Cruz <marcelcruz@live.com>
2017-09-06 14:13:50 +0100

    completed and created timestamps added

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var uuid = require('node-uuid');
+var moment = require('moment');
 
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
@@ -19,18 +20,6 @@ var TodoApp = React.createClass({
     TodoAPI.setTodos(this.state.todos);
   },
 
-  handleToggle: function(id) {
-    var updatedTodos = this.state.todos.map((todo) => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed;
-      }
-
-      return todo;
-    });
-
-    this.setState({ todos: updatedTodos });
-  },
-
   handleAddTodo: function(text) {
     this.setState({
       todos: [
@@ -38,10 +27,25 @@ var TodoApp = React.createClass({
         {
           id: uuid(),
           text: text,
-          completed: false
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
         }
       ]
     });
+  },
+
+  handleToggle: function(id) {
+    var updatedTodos = this.state.todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+        todo.completedAt = todo.completed ? moment().unix() : undefined;
+      }
+
+      return todo;
+    });
+
+    this.setState({ todos: updatedTodos });
   },
 
   handleSearch: function(showCompleted, searchText) {
